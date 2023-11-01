0c5df85dcaa458d80f443871c4b431addfd321cd
Adan Saavedra <adansaavedraa@icloud.com>
2023-10-31 22:16:54 -0500

    finish first theme

--- src/Calculator/index.jsx
+++ src/Calculator/index.jsx
@@ -26,22 +26,22 @@ const Calculator = () => {
           <p>399,981</p>
         </div>
         <div className="container">
-          <div className="item item-1">7</div>
-          <div className="item item-2">8</div>
-          <div className="item item-3">9</div>
-          <div className="item item-4">DEL</div>
-          <div className="item item-5">4</div>
-          <div className="item item-6">5</div>
-          <div className="item item-7">6</div>
-          <div className="item item-8">+</div>
-          <div className="item item-9">1</div>
-          <div className="item item-10">2</div>
-          <div className="item item-11">3</div>
-          <div className="item item-12">-</div>
-          <div className="item item-13">.</div>
+          <div className="item item-7">7</div>
+          <div className="item item-8">8</div>
+          <div className="item item-9">9</div>
+          <div className="item item-del">DEL</div>
+          <div className="item item-4">4</div>
+          <div className="item item-5">5</div>
+          <div className="item item-6">6</div>
+          <div className="item item-plus">+</div>
+          <div className="item item-1">1</div>
+          <div className="item item-2">2</div>
+          <div className="item item-3">3</div>
+          <div className="item item-minus">-</div>
+          <div className="item item-point">.</div>
           <div className="item item-14">0</div>
-          <div className="item item-15">/</div>
-          <div className="item item-16">x</div>
+          <div className="item item-div">/</div>
+          <div className="item item-mult">x</div>
           <div className="item item-reset">RESET</div>
           <div className="item item-equal">=</div>
         </div>
